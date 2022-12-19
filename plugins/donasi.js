46a524981d7d43f7d2bc876b9b1a18fbfa7209c2
khoirul anwar <khoirulazizah35@gmail.com>
2022-12-19 19:37:43 +0700

    memek

---
+++ plugins/donasi.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+let handler = async (m, { conn, text, usedPrefix }) => {
+
+    let img = fs.readFileSync('./thumbnail.jpg');
+    let img2 = `https://telegra.ph/file/cc0288e924b3099a2557a.png`
+    return conn.sendButton(m.chat, 'Donasi Pm Owner aja.\n❏  :v', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: "https://instagram.com/danangvalentp2",
+                mediaType: "VIDEO",
+                title: 'Donasi To JhonBot ',
+                body: bottime,
+                previewType: 0,
+                thumbnail: img
+            }
+        }
+    })
+}
+handler.help = ['donasi']
+
+handler.tags = ['info']
+
+handler.command = /^(donasi|donate)$/i
+
+
+export default handler
