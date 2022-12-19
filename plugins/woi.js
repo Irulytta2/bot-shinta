e8e9689050812277510a5a28984d78d9b83c8877
Irulytta2 <119930118+Irulytta2@users.noreply.github.com>
2022-12-19 21:58:10 +0700

    Update woi.js

--- plugins/woi.js
+++ plugins/woi.js
@@ -32,7 +32,7 @@ contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
                         description: 'anu',
-                        title: `A MIMIR🎧 : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
+                        title: `𝐒𝐇𝐈𝐍𝐓𝐀𝐀-𝐁𝐎𝐓🦋 : ${ohari} Hari ${ojam} Jam ${onet} Menit ${detek} Detik`,
                         body: `Join Grup`,          previewType: 0,
                         thumbnail: await (await fetch(`https://i.ibb.co/5st560k/fake.jpg`)).buffer(),
                         sourceUrl: 'https://chat.whatsapp.com/JytIVwVnOCIFQBA93eZEXu'
